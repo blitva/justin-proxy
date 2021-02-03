cc5caa092181b6ba752dda116d3279a783fc33c4
jrozal <68182930+jrozal@users.noreply.github.com>
2021-02-02 18:50:43 -0800

    Update files for proxy server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const port = 3000;
 app.use(morgan('dev'));
 app.use(cors());
 app.use('/', express.static(__dirname + '/../public'));
+app.use('/:id', express.static(__dirname + '/../public'));
 
 app.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
